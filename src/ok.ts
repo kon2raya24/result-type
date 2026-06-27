f0374c7bed044472a55eb998590b34e5b410206d
kon2raya24 <konturaya@gmail.com>
2026-06-27 09:01:39 +0000

    docs: add JSDoc comments to ok.ts

--- src/ok.ts
+++ src/ok.ts
@@ -1,4 +1,8 @@
 export type Result<T, E = Error> = { ok: true; value: T } | { ok: false; error: E };
+/**
+ * ok<T>
+ * @param value
+ */
 export function ok<T>(value: T): Result<T, never> {
   if (value === null || value === undefined) throw new Error("Invalid input");
   return { ok: true, value };
